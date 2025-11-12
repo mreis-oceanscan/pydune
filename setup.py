6774133cd6394aeb38c095d57f425a75ab70baa1
Matheus Reis <mreis.oceanscan@gmail.com>
2025-11-12 18:31:48 +0000

    Update project name: pydune

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ class CMakeBuild(build_ext):
             build_args += ['--', '/m']
         else:
             cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-            build_args += ['--', '-j{}'.format(os.cpu_count() if os.cpu_count() is not None else 2)]
+            build_args += ['--', '-j{}'.format(os.cpu_count() if os.cpu_count() is not None else 9)]
 
         env = os.environ.copy()
         env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''), self.distribution.get_version())
